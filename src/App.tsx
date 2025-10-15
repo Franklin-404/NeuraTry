fd2f510383e17a04082e08af1562a9cdc442148d
Franklin-404 <wildsauce313@gmail.com>
2025-10-16 00:32:13 +0200

    Scroll To Top update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import ScrollToTop from "./components/ScrollToTop";
 import LoginPage from "./components/LoginPage";
 import TrendingIdeas from "./pages/TrendingIdeas";
 import IdeaDetail from "./pages/IdeaDetail";
@@ -26,6 +27,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<LoginPage />} />
